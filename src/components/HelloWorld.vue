95c337ba5b5d0518900bfcdbdbc4b31bab42194b
qxxx R <1447159064@qq.com>
2025-03-10 17:05:32 +0800

    11

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -23,8 +23,7 @@
           href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router"
           target="_blank"
           rel="noopener"
-          >router</a
-        >
+        ></a>
       </li>
       <li>
         <a
